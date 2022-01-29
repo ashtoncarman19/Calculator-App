24732da7d8f94fd6a670a24ab567c056791a2053
Ashton Carman <ashton.carman@rocketmail.com>
2022-01-29 03:10:55 -0600

    fixed the issue with accidental spaces around the decimal and negative buttons created errors, got a better background, and changed some of the color scheme

--- calculator.js
+++ calculator.js
@@ -1,146 +1,76 @@
-const add = function(a,b) {
-    return a + b;
-        
-    };
-    
-    const subtract = function(a,b) {
-      return a - b;
-        
-    };
-    
-    
-    const sum = function(numbers) {
-      let x = numbers.reduce(function(prev,curr){
-      return curr + prev;
-    },0);
-        return x;
-    };
-    
-    const multiply = function(array) {
-      var sum = 1;
-      for (var i = 0; i < array.length; i++) {
-          sum = sum * array[i];
-      }
-      return sum;
-    }
-
-    const multiplyTwoNumbers = function(a,b){
-      return a * b;
-    }
-    
-    
-    const power = function(a,b) {
-      return Math.pow(a,b)
-    
-    };
-    /*
-    const factorial = function(number) {
-        let result = number;
-      if (number === 0 || number === 1)
-        return 1;
-    
-      while (number > 1 ){
-        number--;
-        result *= number
-      }
-      return result;
-    };
-    */
-    const divide = function(a,b){
-      return a % b;
-}
-//let numKey = document.querySelector("")
-//numKey.addEventListener("click", function(e){
-
-
-function displayClick(Text){
- /* const display = document.querySelector(".display");
-
-  const content = document.createElement('div');
-  content.classList.add('content');
-  content.textContent = String;
-  display.appendChild(content);
-*/
-  //const display = document.getElementById('.display');
-  //display.innerHTML += (a);
-
-  const display = document.querySelector('.display');
-  let node = document.createTextNode (Text)
-  display.appendChild(node);
+  /*
+  This function allows whatever button you click 
+  to add that operator or number to the div with the class 'display'
+  whithout creating a new div
+  */
+  function displayClick(Text){
+    const display = document.querySelector('.display');
+    let node = document.createTextNode (Text)
+    display.appendChild(node);
 }
 
+  /*
+  This function clears the contents of the div class 'display'
+  */
   function clearContent(elementID) {
     document.querySelector(elementID).innerHTML = "";
 }
 
-function getTwoNumbers(){
-  let display = document.querySelector('.display').innerHTML;
-  let firstNumber = parseFloat(display, 10)
-  console.log(firstNumber);
+  /*
+  This function splits the contents of the div 'display' wherever the operator is placed,
+  then takes the two seperate strings that are created and converts them into numbers
+  */
+  function getTwoNumbers(){
+    let display = document.querySelector('.display').innerHTML;
+    let firstNumber = parseFloat(display, 10)
+    console.log(firstNumber);
 
-  if (display.indexOf('+') > -1)
+    /*
+      All of these if statements are searching for which operator was used
+      in the equation that was typed into the 'display' div, 
+      and depending on which operator was used it performs the calculation that was entered
+    */
+      if (display.indexOf('+') > -1)
   {
-      secondPart = display.substring(display.indexOf("+")+ 1);
-      secondNumber = parseFloat(secondPart, 10);
-      result = parseFloat(firstNumber + secondNumber).toPrecision(10)
+        secondPart = display.substring(display.indexOf("+")+ 1);
+        secondNumber = parseFloat(secondPart, 10);
+        result = parseFloat(firstNumber + secondNumber)
     }
   
-  if (display.indexOf('-') > -1)
+      if (display.indexOf('-') > -1)
   {
-      secondPart = display.substring(display.indexOf("-")+ 1);
-      secondNumber = parseFloat(secondPart, 10);
-      result = parseFloat(firstNumber - secondNumber).toPrecision(10)
+        secondPart = display.substring(display.indexOf("-")+ 1);
+        secondNumber = parseFloat(secondPart, 10);
+        result = parseFloat(firstNumber - secondNumber)
     }
 
-  if (display.indexOf('x') > -1)
+      if (display.indexOf('x') > -1)
   {
-      secondPart = display.substring(display.indexOf("x")+ 1);
-      secondNumber = parseFloat(secondPart, 10);
-      result = parseFloat(firstNumber * secondNumber).toPrecision(10)
+        secondPart = display.substring(display.indexOf("x")+ 1);
+        secondNumber = parseFloat(secondPart, 10);
+        result = parseFloat(firstNumber * secondNumber)
     }
     
-  if (display.indexOf('/') > -1)
+      if (display.indexOf('÷') > -1)
   {
-      secondPart = display.substring(display.indexOf("/")+ 1);
-      secondNumber = parseFloat(secondPart, 10);
-      result = parseFloat(firstNumber / secondNumber).toPrecision(10)
+        secondPart = display.substring(display.indexOf("÷")+ 1);
+        secondNumber = parseFloat(secondPart, 10);
+        result = parseFloat(firstNumber / secondNumber).toPrecision(10)
     }
 
-  if (display.indexOf('^') > -1)
+      if (display.indexOf('^') > -1)
   {
-      secondPart = display.substring(display.indexOf("^")+ 1);
-      secondNumber = parseFloat(secondPart, 10);
-      result = Math.pow(firstNumber,secondNumber)
+        secondPart = display.substring(display.indexOf("^")+ 1);
+        secondNumber = parseFloat(secondPart, 10);
+        result = Math.pow(firstNumber,secondNumber)
     }
   
   console.log(secondNumber)
   console.log(result)
+
+  /*
+      This displayes the new result in the 'display' div 
+  */
   document.querySelector('.display').innerHTML = result
 
-
-
- /* function solve(){
-    if (display.indexOf('+') > -1){
-        result = firstNumber + secondNumber
-    }
-  
-    if (display.indexOf('-') > -1){
-        result = subtract(a,b)
-    }
-  
-    if (display.indexOf('x') > -1){
-        result = multiplyTwoNumbers(a,b)
-    }
-  
-    if (display.indexOf('/') > -1){
-        result = divide(a,b)
-    }
-  
-    if (display.indexOf('^') > -1){
-        result = power(a,b)
-    }
-  
-    console.log(result)
-  }
-  */
 }
