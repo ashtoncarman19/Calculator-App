ebe36976c3827adb4eff54da282f6a9d31cb4139
Ashton Carman <ashton.carman@rocketmail.com>
2022-01-31 02:31:07 -0600

    complete calculator renovation with Css Grid and changing the colors, the backspace button still doesnt work but everything else does and it computes correctly.

--- calculator.js
+++ calculator.js
@@ -4,30 +4,37 @@
   whithout creating a new div
   */
   function displayClick(Text){
-    const display = document.querySelector('.display');
+    const display = document.querySelector('.output');
     let node = document.createTextNode (Text)
     display.appendChild(node);
 }
 
   /*
-  This function clears the contents of the div class 'display'
+  This function clears the contents of the div class 'output'
   */
   function clearContent(elementID) {
     document.querySelector(elementID).innerHTML = "";
 }
 
   /*
-  This function splits the contents of the div 'display' wherever the operator is placed,
+  This function deletes the latest number or symbol in
+  the contents of the div class 'output'
+  */
+  function backspace(){
+    document.querySelector('.output').innerHTML.slice(0, -1)
+    }
+  /*
+  This function splits the contents of the div 'output' wherever the operator is placed,
   then takes the two seperate strings that are created and converts them into numbers
   */
   function getTwoNumbers(){
-    let display = document.querySelector('.display').innerHTML;
+    let display = document.querySelector('.output').innerHTML;
     let firstNumber = parseFloat(display, 10)
     console.log(firstNumber);
 
     /*
       All of these if statements are searching for which operator was used
-      in the equation that was typed into the 'display' div, 
+      in the equation that was typed into the 'output' div, 
       and depending on which operator was used it performs the calculation that was entered
     */
       if (display.indexOf('+') > -1)
@@ -55,7 +62,7 @@
   {
         secondPart = display.substring(display.indexOf("÷")+ 1);
         secondNumber = parseFloat(secondPart, 10);
-        result = parseFloat(firstNumber / secondNumber).toPrecision(10)
+        result = parseFloat(firstNumber / secondNumber).toPrecision(7)
     }
 
       if (display.indexOf('^') > -1)
@@ -71,6 +78,6 @@
   /*
       This displayes the new result in the 'display' div 
   */
-  document.querySelector('.display').innerHTML = result
+  document.querySelector('.output').innerHTML = result
 
 }
